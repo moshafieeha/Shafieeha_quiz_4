0ec8bc96417c403251f6b1006d3c5f30b92c4afd
moshafieeha <m.shafieeha@gmail.com>
2023-01-27 12:13:39 -0800

    add 4

--- quiz-04-4/main.js
+++ quiz-04-4/main.js
@@ -1,39 +1,49 @@
-const tableHead = document.querySelector('thead');
-const tableBody = document.querySelector('tbody');
+const tableHead = document.querySelector("thead");
+const tableBody = document.querySelector("tbody");
+
+let url_string = window.location.href;
+let url = new URL(url_string);
+let filterKey;
+let filterValue;
 
 const renderTable = () => {
-	tableHead.innerHTML = `
+  tableHead.innerHTML = `
 		<tr>
-			<th scope="col">No.</th>
-			<th scope="col">Tour ID</th>
-			<th scope="col">Name</th>
-			<th scope="col">Location</th>
-			<th scope="col">Price</th>
-			<th scope="col">Group Size</th>
-			<th scope="col">difficulty</th>
-			<th scope="col">duration</th>
-			<th scope="col">Ratings Average</th>
-			<th scope="col">Ratings Quantity</th>
+			<th>No.</th>
+			<th>Tour ID</th>
+			<th>Name</th>
+			<th>Location</th>
+			<th>Price</th>
+			<th>Group Size</th>
+			<th>difficulty</th>
+			<th>duration</th>
+			<th>Ratings Average</th>
+			<th>Ratings Quantity</th>
 		</tr>`;
 
-	let rowCount = 1;
-	for (const tour of tours) {
-		tableBody.innerHTML += `
-			<tr>
-				<th scope="row">${rowCount}</th>
-				<td>${tour.id}</td>
-				<td>${tour.name}</td>
-				<td>${tour.location}</td>
-				<td>${tour.price}</td>
-				<td>${tour.maxGroupSize}</td>
-				<td>${tour.difficulty}</td>
-				<td>${tour.duration}</td>
-				<td>${tour.ratingsAverage}</td>
-				<td>${tour.ratingsQuantity}</td>
-			</tr>`;
+  let rowCount = 1;
 
-		rowCount += 1;
-	}
+  filterKey = url.search.split("?")[1].split("=")[0];
+  filterValue = url.search.split("?")[1].split("=")[1];
+
+  for (const tour of tours) {
+    if (!filterKey || tour[filterKey] === filterValue) {
+      tableBody.innerHTML += `
+				<tr>
+					<td>${rowCount}</td>
+					<td>${tour.id}</td>
+					<td>${tour.name}</td>
+					<td>${tour.location}</td>
+					<td>${tour.price}</td>
+					<td>${tour.maxGroupSize}</td>
+					<td>${tour.difficulty}</td>
+					<td>${tour.duration}</td>
+					<td>${tour.ratingsAverage}</td>
+					<td>${tour.ratingsQuantity}</td>
+				</tr>`;
+      rowCount++;
+    }
+  }
 };
 
 renderTable();
